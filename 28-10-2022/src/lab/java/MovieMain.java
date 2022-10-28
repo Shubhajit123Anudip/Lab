9adc1a80bf19cbac051e00cebba77af99074a972
Shubhajit123Anudip <111556510+Shubhajit123Anudip@users.noreply.github.com>
2022-10-28 12:35:35 +0530

    Add files via upload

---
+++ 28-10-2022/src/lab/java/MovieMain.java
@@ -0,0 +1,15 @@
+package lab.java;
+
+public class MovieMain {
+
+	public static void main(String[] args) {
+		Movie m1=new Movie();
+		Movie m2=new Movie();
+		m1.setMovie(150,"Kantara","10am","12pm");
+		m1.getMovie();
+		m1.setMovie(150,"KGF","6pm","8pm");
+		m1.getMovie();
+
+	}
+
+}
