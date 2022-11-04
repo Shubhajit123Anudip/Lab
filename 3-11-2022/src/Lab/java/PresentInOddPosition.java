6f514e4a5b131793e27b1f00f29705cd59cfb470
Shubhajit Maji <111556510+Shubhajit123Anudip@users.noreply.github.com>
2022-11-04 10:51:13 +0530

    Add files via upload

---
+++ 3-11-2022/src/Lab/java/PresentInOddPosition.java
@@ -0,0 +1,14 @@
+package Lab.java;
+
+public class PresentInOddPosition {
+
+	public static void main(String[] args) {
+		String[] arr={"a","b","c","d","e"};
+       for(int i=0;i<arr.length;i=i+2) {
+    	   System.out.println("Present in odd position are:"+arr[i]);
+    	   
+    	   
+       }
+	}
+
+}
